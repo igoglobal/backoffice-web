b4a71ae62b2321fd8379fcbfe77d5c996c3e1b7a
talented_vicky <victorotubure7@gmail.com>
2024-10-22 14:31:59 +0100

    created react project with vite, tailwindcss, zustand and typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='p-4 bg-blue-500'>
+        <h3 className='text-white'> Welcome y'all</h3>
+      </div>
+    </>
+  )
+}
+
+export default App
